b88337ab913f4e9deada42309864af5ed84f589d
Cesar Baudi <cesarbaudi1@gmail.com>
2021-12-03 22:18:17 -0400

    React App created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <TodoCounter />
+      <h2>Has completado 2 de 3 TODOs</h2>
+
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
+    </>
+  );
+}
+
+export default App;
