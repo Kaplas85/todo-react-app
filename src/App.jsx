140b1a5f5b72f5ed0f8d0e80c80a68ac35bf74d3
Cesar Baudi <cesarbaudi1@gmail.com>
2021-12-03 22:41:09 -0400

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import TodoCounter from "./components/TodoCounter";
+import TodoSearch from "./components/TodoSearch";
+import CreateTodoButton from "./components/CreateTodoButton";
+import TodoList from "./components/TodoList";
+import TodoItem from "./components/TodoItem";
+
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar curso de intro a React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
+
 function App() {
   return (
     <>
       <TodoCounter />
-      <h2>Has completado 2 de 3 TODOs</h2>
-
       <TodoSearch />
+
       <TodoList>
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
       </TodoList>
       <CreateTodoButton />
     </>
---
+++ src/components/TodoCounter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TodoCounter() {
+  return <h2>Has completado 2 de 3 TODOs</h2>;
+}
